0fd54c2ad11c76ae4425dad002aab48e82d5bde2
Michel Silva <michelalmeidasilva@outlook.pt>
2020-11-11 03:55:38 -0300

    Sprint-02 finalizada pelo grupo do Tiago

---
+++ src/expressoesregulares/Input.java
@@ -0,0 +1,222 @@
+package expressoesregulares;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Input {
+    static Pattern patternUnion = Pattern.compile("([\\wαβγδζηθι]+)(\\|)([\\wαβγδζηθι]+)");
+    static Pattern patternConcatenate = Pattern.compile("([\\wαβγδζηθι]+)(\\.)([\\wαβγδζηθι]+)");
+    static Pattern patternKleeneClosure = Pattern.compile("([\\wαβγδζηθι]+)(\\*)");
+    static String expression = new String();
+
+    // α β γ δ ζ η θ ι
+    static ArrayList<String> varList = new ArrayList<String>(Arrays.asList("0", "0", "0", "0", "0", "0", "0", "0"));
+    static ArrayList<String> alpha = new ArrayList<String>();
+    static ArrayList<String> beta = new ArrayList<String>();
+    static ArrayList<String> gama = new ArrayList<String>();
+    static ArrayList<String> delta = new ArrayList<String>();
+    static ArrayList<String> zeta = new ArrayList<String>();
+    static ArrayList<String> eta = new ArrayList<String>();
+    static ArrayList<String> theta = new ArrayList<String>();
+    static ArrayList<String> iota = new ArrayList<String>();
+
+    public static ArrayList<String> testCalc(String inp) {
+        ArrayList<String> out = new ArrayList<String>();
+        expression = inp;
+        int validate = 0;
+        while (validate != 1) {
+            // System.out.println("pass "+expression);
+            if (patternKleeneClosure.matcher(expression).find()) {
+                out.addAll(findClosureKleene());
+            } else if (patternUnion.matcher(expression).find()) {
+                out.addAll(findUnion());
+            } else if (patternConcatenate.matcher(expression).find()) {
+                out.addAll(findConcatenate());
+            } else {
+                validate = 1;
+            }
+        }
+        
+        ArrayList<String> list = new ArrayList<String>(new HashSet<>(out));
+        Collections.sort(list);
+        list.add(0, "ε");
+        return list;
+    }
+
+    static ArrayList<String> defineVariable(String var) {
+        ArrayList<String> varList = new ArrayList<String>();
+        switch (var) {
+            case "α":
+                varList.addAll(alpha);
+                break;
+
+            case "β":
+                varList.addAll(beta);
+                break;
+
+            case "γ":
+                varList.addAll(gama);
+                break;
+
+            case "δ":
+                varList.addAll(delta);
+                break;
+            case "ζ":
+                varList.addAll(zeta);
+                break;
+
+            case "η":
+                varList.addAll(eta);
+                break;
+
+            case "θ":
+                varList.addAll(theta);
+                break;
+
+            case "ι":
+                varList.addAll(iota);
+                break;
+
+            default:
+                varList.addAll(Arrays.asList(var));
+                break;
+        }
+        return varList;
+    }
+
+    static String findVariable(ArrayList<String> l1) {
+        String output = "";
+        int ok = 0;
+        int i = 0;
+        while (ok == 0) {
+            if (varList.get(i) == "0") {
+                ok = 1;
+            } else {
+                i++;
+            }
+        }
+        varList.set(i, "1");
+        switch (i) {
+            case 0:
+                alpha.addAll(l1);
+                output = "α";
+                break;
+
+            case 1:
+                beta.addAll(l1);
+                output = "β";
+                break;
+
+            case 2:
+                gama.addAll(l1);
+                output = "γ";
+                break;
+
+            case 3:
+                delta.addAll(l1);
+                output = "δ";
+                break;
+
+            case 4:
+                alpha.addAll(l1);
+                output = "ζ";
+                break;
+
+            case 5:
+                beta.addAll(l1);
+                output = "η";
+                break;
+
+            case 6:
+                gama.addAll(l1);
+                output = "θ";
+                break;
+
+            case 7:
+                delta.addAll(l1);
+                output = "ι";
+                break;
+
+            default:
+                output = "";
+                break;
+        }
+        return output;
+    }
+
+    public static ArrayList<String> findUnion() {
+        ArrayList<String> output = new ArrayList<String>();
+        Matcher matcher = patternUnion.matcher(expression);
+        while (matcher.find()) {
+            // String match = matcher.group();
+            String group1 = matcher.group(1);
+            String group2 = matcher.group(3);
+
+            output.addAll(Operators.union(defineVariable(group1), defineVariable(group2)));
+            expression = expression.replaceFirst("([\\wαβγδζηθι\\(]+)(\\|)([\\wαβγδζηθι\\)]+)", findVariable(output));
+
+            // printAux(matcher.start(), matcher.end(), match);
+
+            break;
+        }
+        // printVariables();
+        return output;
+    }
+
+    public static ArrayList<String> findConcatenate() {
+        ArrayList<String> output = new ArrayList<String>();
+        Matcher matcher = patternConcatenate.matcher(expression);
+        while (matcher.find()) {
+            // String match = matcher.group();
+            String group1 = matcher.group(1);
+            String group2 = matcher.group(3);
+
+            output.addAll(Operators.concatenate(defineVariable(group1), defineVariable(group2)));
+            expression = expression.replaceFirst("([\\wαβγδζηθι\\(]+)(\\.)([\\wαβγδζηθι\\)]+)", findVariable(output));
+
+            // printAux(matcher.start(), matcher.end(), match);
+
+            break;
+        }
+        // printVariables();
+        return output;
+    }
+
+    public static ArrayList<String> findClosureKleene() {
+        ArrayList<String> output = new ArrayList<String>();
+        Matcher matcher = patternKleeneClosure.matcher(expression);
+        while (matcher.find()) {
+            // String match = matcher.group();
+            String group1 = matcher.group(1);
+
+            output.addAll(Operators.kleeneClosure(defineVariable(group1)));
+            expression = expression.replaceFirst("([\\wαβγδζηθι]+)(\\*)", findVariable(output));
+
+            // printAux(matcher.start(), matcher.end(), match);
+
+            break;
+        }
+        // printVariables();
+        return output;
+    }
+
+    static void printVariables() {
+        System.out.println("α = " + alpha);
+        System.out.println("β = " + beta);
+        System.out.println("γ = " + gama);
+        System.out.println("δ = " + delta);
+        System.out.println("ζ = " + zeta);
+        System.out.println("η = " + eta);
+        System.out.println("θ = " + theta);
+        System.out.println("ι = " + iota);
+    }
+
+    static void printAux(int start, int end, String match) {
+        System.out.printf("%s - [%d,%d] \t => %s\n", match, start, end, expression);
+    }
+
+}
---
+++ src/expressoesregulares/InputTest.java
@@ -0,0 +1,71 @@
+package expressoesregulares;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+    
+public class InputTest {
+
+    // input test
+    String test1 = "1*";
+    String test2 = "1|0";
+    String test3 = "0|1*";
+    String test4 = "(0|1)*";
+    String test5 = "10.11";
+    String test6 = "(((0|1).10).(((0|1).(1.1)).1)*)";
+    String test7 = "(0|1*).(0|1)*";
+
+    // output expected
+    ArrayList<String> test1Expected = new ArrayList<String>(Arrays.asList("ε", "1", "11", "111", "1111"));
+    ArrayList<String> test2Expected = new ArrayList<String>(Arrays.asList("ε", "0", "1"));
+    ArrayList<String> test3Expected = new ArrayList<String>(Arrays.asList("ε", "0", "1", "11", "111", "1111"));
+    ArrayList<String> test4Expected = new ArrayList<String>(Arrays.asList("ε", "0", "00", "000", "0000", "0001", "001", "0010", "0011", "01", "010", "0100", "0101", "011", "0110", "0111", "1", "10", "100", "1000", "1001", "101", "1010", "1011", "11", "110", "1100", "1101", "111", "1110", "1111"));
+    ArrayList<String> test5Expected = new ArrayList<String>(Arrays.asList("ε", "1011"));
+    ArrayList<String> test6Expected = new ArrayList<String>(Arrays.asList("ε", "0", "010", "0100", "0101", "1", "11", "110", "1100", "1101"));
+    ArrayList<String> test7Expected = new ArrayList<String>(Arrays.asList("ε", "0", "00", "000", "0000", "00000", "00001", "0001", "00010", "00011", "001", "0010", "00100", "00101", "0011", "00110", "00111", "01", "010", "0100", "01000", "01001", "0101", "01010", "01011", "011", "0110", "01100", "01101", "0111", "01110", "01111", "1", "10", "100", "1000", "10000", "10001", "1001", "10010", "10011", "101", "1010", "10100", "10101", "1011", "10110", "10111", "11", "110", "1100", "11000", "110000", "110001", "11001", "110010", "110011", "1101", "11010", "110100", "110101", "11011", "110110", "110111", "111", "1110", "11100", "111000", "1110000", "1110001", "111001", "1110010", "1110011", "11101", "111010", "1110100", "1110101", "111011", "1110110", "1110111", "1111", "11110", "111100", "1111000", "11110000", "11110001", "1111001", "11110010", "11110011", "111101", "1111010", "11110100", "11110101", "1111011", "11110110", "11110111", "11111", "111110", "1111100", "11111000", "11111001", "1111101", "11111010", "11111011", "111111", "1111110", "11111100", "11111101", "1111111", "11111110", "11111111"));
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void executeTest1() {
+        assertEquals(test1Expected, Input.testCalc(test1));
+    }
+
+    @Test
+    public void executeTest2() {
+        assertEquals(test2Expected, Input.testCalc(test2));
+    }
+
+    @Test
+    public void executeTest3() {
+        assertEquals(test3Expected, Input.testCalc(test3));
+    }
+
+    @Test
+    public void executeTest4() {
+        assertEquals(test4Expected, Input.testCalc(test4));
+    }
+
+    @Test
+    public void executeTest5() {
+        assertEquals(test5Expected, Input.testCalc(test5));
+    }
+
+    @Test
+    public void executeTest6() {
+        assertEquals(test6Expected, Input.testCalc(test6));
+    }
+
+    @Test
+    public void executeTest7() {
+        assertEquals(test7Expected, Input.testCalc(test7));
+    }
+
+}
+    
