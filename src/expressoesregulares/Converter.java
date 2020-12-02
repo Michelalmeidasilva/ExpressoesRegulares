3e1778178562aaa8c650370e31b375897ab4f59f
Michel Silva <michelalmeidasilva@outlook.pt>
2020-12-02 16:14:24 -0300

    clase para conversão de regex to afnd

--- src/expressoesregulares/Converter.java
+++ src/expressoesregulares/Converter.java
@@ -1,19 +1,86 @@
 package expressoesregulares;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Vector;
+
+
+class Parenteses {
+  int posIni;
+  int posFim;
+}
+
+// [1][10]
+// [2][]
+// [3][4]
+// [5][6]
+// [7][]
+// [11][]
+// entrada: a((()()()))()
+
 public class Converter {
   private String entrada;
-
   public Converter(String entrada){
     this.entrada = entrada;
   }
+  boolean temParenteses = false;
+  String entradaAuxiliar;
+  //"((ab(a|b))(100|ε))";
+  List<Parenteses> parentesesList = new ArrayList<>();
+
 
   public void transformRegexToAFND(){
-    /**
-     * codigo para pegar a entrada
-     * e transformar para parametros de um AFND
-     */
+    boolean inicio = true;
+    entradaAuxiliar = entrada;
+    while(temParenteses || inicio == true){
+      inicio = false;
+      temParenteses = procuraParenteses();
+    }
 
-    createAFND();
+
+//    procuraParentesesMatriz();
+//    createAFND();
+  }
+
+  private void procuraParentesesMatriz() {
+
+  }
+
+    private boolean procuraParenteses() {
+    int qtdParentenses = 0;
+    boolean primeiro= false;
+    StringBuilder sb = new StringBuilder(entradaAuxiliar);
+
+    Parenteses parente = new Parenteses();
+
+    for (int i = 0; i < this.entrada.length(); i++) {
+
+      if(this.entradaAuxiliar.charAt(i)== '('){
+        temParenteses = true;
+        if(primeiro == false){
+          sb.deleteCharAt(i);
+          primeiro = true;
+          parente.posIni = i;
+        }
+        qtdParentenses++;
+      }
+
+      if(this.entradaAuxiliar.charAt(i) == ')'){
+        qtdParentenses--;
+      }
+
+      if(qtdParentenses == 0){
+        parente.posFim = i;
+        parentesesList.add(parente);
+        sb.deleteCharAt(i -1);
+
+        primeiro = false;
+      }
+    }
+
+    entradaAuxiliar = sb.toString();
+    return temParenteses;
   }
 
   public void createAFND(){
--- test/expressoesregulares/ConverterTest.java
+++ test/expressoesregulares/ConverterTest.java
@@ -12,13 +12,11 @@ public class ConverterTest {
   @Test
   public void whatever () throws IOException {
     ER expressoesRegulares = new ER();
-    ArrayList<String> out = new ArrayList<String>();
-    out = expressoesRegulares.GerarPalavras(20, "(10)(10|ε)(11|ε)(100|ε)*(1000)");
-    for (int i = 0; i < out.size(); i++) {
-      System.out.println(out.get(i) );
-    }
-    String entrada = "(10)(10|ε)(11|ε)(100|ε)*(1000)";
+
+    String entrada = "((ab)(100|ε))";
+
     Converter converter = new Converter (entrada);
+    converter.transformRegexToAFND();
   }
 
 
