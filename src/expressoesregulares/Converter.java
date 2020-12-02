d255cb8618f5615b651820ae4fb0d01534db7148
Michel Silva <michelalmeidasilva@outlook.pt>
2020-12-02 20:44:47 -0300

    implementação da lógica de percorrer uma entrada para mapear os parentess

--- src/expressoesregulares/Converter.java
+++ src/expressoesregulares/Converter.java
@@ -1,47 +1,83 @@
 package expressoesregulares;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.*;
 
 
 class Parenteses {
   int posIni;
   int posFim;
+  int nivel;
+  String conteudo;
+
+  public Parenteses(){
+
+  }
+  public Parenteses (int posIni, int posFim, String conteudo, int nivel){
+    this.posIni = posIni;
+    this.posFim = posFim;
+    this.conteudo = conteudo;
+    this.nivel = nivel;
+  }
 }
 
-class Posicao {
-  boolean marcacao;
-  String value;
-}
+
 public class Converter {
-  List<Parenteses> parentesesList = new ArrayList<>();
+  List<Parenteses> mapa = new ArrayList<>();
 
   public void transformRegexToAFND(String entrada){
       procuraParenteses(entrada);
+
+
   }
 
+
+  /**
+   * @param entrada
+   * Mapeamento de parenteses
+   * da entrada por nivel seguindo a logica
+   * de precedencia de parenteses, quanto mais profundo
+   * o parenteses maior o  nivel acoplado ao objeto da classe Parenteses
+   */
   private void procuraParenteses(String entrada) {
+
     Map<Integer , Boolean> posicoes = new HashMap< Integer, Boolean>();
+    Parenteses parenteses = new Parenteses();
+    int nivel = 0;
+
+    mapa.add(new Parenteses (0 ,( entrada.length() -1) , entrada, 0));
+
     for (int i =0; i < entrada.length(); i ++){
       posicoes.put( i, false);
     }
 
-    Parenteses parenteses = new Parenteses();
-
     for (int i = 0; i < entrada.length(); i++) {
+      char atual= entrada.charAt(i);
 
-      if(entrada.charAt(i) == '(' && posicoes.get(i) == false){
-         parenteses.posIni = i;
-      }
+      if(atual == '(') nivel++;
 
-      if(entrada.charAt(i) == ')' && posicoes.get(i) == false){
+      if(atual == '(' && posicoes.get(i) == false) parenteses.posIni = i;
+
+      if(atual == ')' && posicoes.get(i) == false){
         parenteses.posFim = i;
+        parenteses.nivel = nivel;
+        parenteses.conteudo = entrada.substring(parenteses.posIni + 1, parenteses.posFim);
         posicoes.put(parenteses.posIni, true);
         posicoes.put(parenteses.posFim, true);
-        parentesesList.add(parenteses);
+        mapa.add(parenteses);
         parenteses = new Parenteses();
-        i = 0;
+        i = -1;
+        nivel = 0;
       }
+
+      if(atual == ')' && nivel > 0)  nivel --;
     }
+  }
+
+
+  public boolean checaParenteses() throws NotImplementedException {
+    throw new NotImplementedException();
   }
 
   public void createAFND(){
--- test/expressoesregulares/ConverterTest.java
+++ test/expressoesregulares/ConverterTest.java
@@ -2,24 +2,53 @@ package expressoesregulares;
 
 import org.junit.Test;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-
 public class ConverterTest {
 
   @Test
-  public void whatever () throws IOException {
-    ER expressoesRegulares = new ER();
+  public void whatever () throws Exception {
 
-    String entrada = "((ab)(100|ε))";
-
-    Converter converter = new Converter (entrada);
-    converter.transformRegexToAFND();
   }
 
+  @Test
+  public void with0Parenteses(){
+    String entrada = "a|b";
+    Converter converter = new Converter ();
+    converter.transformRegexToAFND(entrada);
+  }
 
+  @Test
+  public void with1Parenteses(){
+    String entrada = "(a|b) | (ab)";
+    Converter converter = new Converter ();
+    converter.transformRegexToAFND(entrada);
+  }
 
+  @Test
+  public void with2Parenteses(){
+    String entrada = "((ab)(100|ε)(a|b))|(ab)";
+    Converter converter = new Converter ();
+    converter.transformRegexToAFND(entrada);
+  }
+
+  @Test
+  public void with2Expressions(){
+    String entrada = "(ab)| ((ab)(100|ε)(a|b))";
+    Converter converter = new Converter ();
+    converter.transformRegexToAFND(entrada);
+  }
+
+  @Test
+  public void with3Parenteses(){
+    String entrada = "(((ab)(100|ε)(a|b))) |(ab)";
+    Converter converter = new Converter ();
+    converter.transformRegexToAFND(entrada);
+  }
+
+  @Test
+  public void piorCenario(){
+    String entrada = "(((ab)))| ((ab)(100|ε)(a|b))";
+    Converter converter = new Converter ();
+    converter.transformRegexToAFND(entrada);
+  }
 
 }
